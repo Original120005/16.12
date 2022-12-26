4aa28ca47d948996e6589bcf1bcecd2fa9446f8a
Дима <@DESKTOP-TC925PR>
2022-12-26 19:08:05 +0200

    Add project files.

---
+++ 16.12/16.12.cpp
@@ -0,0 +1,126 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define WIN32_LEAN_AND_MEAN
+
+#include <ws2tcpip.h>
+#include <windows.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+#pragma comment (lib, "Ws2_32.lib")
+
+#define DEFAULT_BUFLEN 4096
+
+#define SERVER_IP "127.0.12.1"
+#define DEFAULT_PORT "2222"
+
+SOCKET client_socket;
+string nickname;
+HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+DWORD WINAPI Sender(void* param)
+{
+    cout << "Enter your nickname pls : ";
+    cin >> nickname;
+    nickname += ": ";
+
+    while (true) {
+        string result;
+        string query;
+        getline(cin, query);
+        result += nickname;
+        result += query;
+        send(client_socket, result.c_str(), result.size(), 0);
+    }
+}
+
+DWORD WINAPI Receiver(void* param)
+{
+    while (true) {
+        char response[DEFAULT_BUFLEN];
+        int result = recv(client_socket, response, DEFAULT_BUFLEN, 0);
+        response[result] = '\0';
+
+        // cout << "...\nYou have new response from server: " << response << "\n";
+        cout << response << "\n";
+        // cout << "Please insert your query for server: ";
+    }
+}
+
+BOOL ExitHandler(DWORD whatHappening)
+{
+    switch (whatHappening)
+    {
+    case CTRL_C_EVENT: // closing console by ctrl + c
+    case CTRL_BREAK_EVENT: // ctrl + break
+    case CTRL_CLOSE_EVENT: // closing the console window by X button
+        return(TRUE);
+        break;
+    default:
+        return FALSE;
+    }
+}
+
+int main()
+{
+    // îáðàáîò÷èê çàêðûòèÿ îêíà êîíñîëè
+    //SetConsoleCtrlHandler((PHANDLER_ROUTINE)ExitHandler, true);
+
+    system("title Client");
+
+    // initialize Winsock
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed with error: %d\n", iResult);
+        return 1;
+    }
+    addrinfo hints = {};
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    // ðàçðåøèòü àäðåñ ñåðâåðà è ïîðò
+    addrinfo* result = nullptr;
+    iResult = getaddrinfo(SERVER_IP, DEFAULT_PORT, &hints, &result);
+    if (iResult != 0) {
+        printf("getaddrinfo failed with error: %d\n", iResult);
+        WSACleanup();
+        return 2;
+    }
+
+    addrinfo* ptr = nullptr;
+    // ïûòàòüñÿ ïîäêëþ÷èòüñÿ ê àäðåñó, ïîêà íå óäàñòñÿ
+    for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+        // ñîçäàòü ñîêåò íà ñòîðîíå êëèåíòà äëÿ ïîäêëþ÷åíèÿ ê ñåðâåðó
+        client_socket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+
+        if (client_socket == INVALID_SOCKET) {
+            printf("socket failed with error: %ld\n", WSAGetLastError());
+            WSACleanup();
+            return 3;
+        }
+
+        // connect to server
+        iResult = connect(client_socket, ptr->ai_addr, (int)ptr->ai_addrlen);
+        if (iResult == SOCKET_ERROR) {
+            closesocket(client_socket);
+            client_socket = INVALID_SOCKET;
+            continue;
+        }
+        break;
+    }
+
+    freeaddrinfo(result);
+
+    if (client_socket == INVALID_SOCKET) {
+        printf("Unable to connect to server!\n");
+        WSACleanup();
+        return 5;
+    }
+
+    CreateThread(0, 0, Sender, 0, 0, 0);
+    CreateThread(0, 0, Receiver, 0, 0, 0);
+
+    Sleep(INFINITE);
+}
---
+++ 16.12/Server.cpp
@@ -0,0 +1,152 @@
+﻿#include <winsock2.h>
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define MAX_CLIENTS 10
+#define DEFAULT_BUFLEN 4096
+
+#pragma comment(lib, "ws2_32.lib") // Winsock library
+#pragma warning(disable:4996) // îòêëþ÷àåì ïðåäóïðåæäåíèå _WINSOCK_DEPRECATED_NO_WARNINGS
+
+SOCKET server_socket;
+
+vector<string> history;
+
+int main() {
+	system("title Server");
+
+	puts("Start server... DONE.");
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+		printf("Failed. Error Code: %d", WSAGetLastError());
+		return 1;
+	}
+
+	// create a socket
+	if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+		printf("Could not create socket: %d", WSAGetLastError());
+		return 2;
+	}
+	// puts("Create socket... DONE.");
+
+	// prepare the sockaddr_in structure
+	sockaddr_in server;
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(8888);
+
+	// bind socket
+	if (bind(server_socket, (sockaddr*)&server, sizeof(server)) == SOCKET_ERROR) {
+		printf("Bind failed with error code: %d", WSAGetLastError());
+		return 3;
+	}
+
+	// puts("Bind socket... DONE.");
+
+	// ñëóøàòü âõîäÿùèå ñîåäèíåíèÿ
+	listen(server_socket, MAX_CLIENTS);
+
+	// ïðèíÿòü è âõîäÿùåå ñîåäèíåíèå
+	puts("Server is waiting for incoming connections...\nPlease, start one or more client-side app.");
+
+	// ðàçìåð íàøåãî ïðèåìíîãî áóôåðà, ýòî äëèíà ñòðîêè
+	// íàáîð äåñêðèïòîðîâ ñîêåòîâ
+	// fd means "file descriptors"
+	fd_set readfds; // https://docs.microsoft.com/en-us/windows/win32/api/winsock/ns-winsock-fd_set
+	SOCKET client_socket[MAX_CLIENTS] = {};
+
+	while (true) {
+		// î÷èñòèòü ñîêåò fdset
+		FD_ZERO(&readfds);
+
+		// äîáàâèòü ãëàâíûé ñîêåò â fdset
+		FD_SET(server_socket, &readfds);
+
+		// äîáàâèòü äî÷åðíèå ñîêåòû â fdset
+		for (int i = 0; i < MAX_CLIENTS; i++)
+		{
+			SOCKET s = client_socket[i];
+			if (s > 0) {
+				FD_SET(s, &readfds);
+			}
+		}
+
+		// äîæäèòåñü àêòèâíîñòè íà ëþáîì èç ñîêåòîâ, òàéì-àóò ðàâåí NULL, ïîýòîìó æäèòå áåñêîíå÷íî
+		if (select(0, &readfds, NULL, NULL, NULL) == SOCKET_ERROR) {
+			printf("select function call failed with error code : %d", WSAGetLastError());
+			return 4;
+		}
+
+		// åñëè ÷òî-òî ïðîèçîøëî íà ìàñòåð-ñîêåòå, òî ýòî âõîäÿùåå ñîåäèíåíèå
+		SOCKET new_socket; // íîâûé êëèåíòñêèé ñîêåò
+		sockaddr_in address;
+		int addrlen = sizeof(sockaddr_in);
+		if (FD_ISSET(server_socket, &readfds)) {
+			if ((new_socket = accept(server_socket, (sockaddr*)&address, &addrlen)) < 0) {
+				perror("accept function error");
+				return 5;
+			}
+
+			for (int i = 0; i < history.size(); i++)
+			{
+				cout << history[i] << "\n";
+				send(new_socket, history[i].c_str(), history[i].size(), 0);
+			}
+
+			// èíôîðìèðîâàòü ñåðâåðíóþ ñòîðîíó î íîìåðå ñîêåòà - èñïîëüçóåòñÿ â êîìàíäàõ îòïðàâêè è ïîëó÷åíèÿ
+			printf("New connection, socket fd is %d, ip is: %s, port: %d\n", new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+
+			// äîáàâèòü íîâûé ñîêåò â ìàññèâ ñîêåòîâ
+			for (int i = 0; i < MAX_CLIENTS; i++) {
+				if (client_socket[i] == 0) {
+					client_socket[i] = new_socket;
+					printf("Adding to list of sockets at index %d\n", i);
+					break;
+				}
+			}
+		}
+
+
+		// åñëè êàêîé-òî èç êëèåíòñêèõ ñîêåòîâ îòïðàâëÿåò ÷òî-òî
+		for (int i = 0; i < MAX_CLIENTS; i++)
+		{
+			SOCKET s = client_socket[i];
+			// åñëè êëèåíò ïðèñóòñòâóåò â ñîêåòàõ ÷òåíèÿ
+			if (FD_ISSET(s, &readfds))
+			{
+				// ïîëó÷èòü ðåêâèçèòû êëèåíòà
+				getpeername(s, (sockaddr*)&address, (int*)&addrlen);
+
+				// ïðîâåðüòå, áûëî ëè îíî íà çàêðûòèå, à òàêæå ïðî÷èòàéòå âõîäÿùåå ñîîáùåíèå
+				// recv íå ïîìåùàåò íóëåâîé òåðìèíàòîð â êîíåö ñòðîêè (â òî âðåìÿ êàê printf %s ïðåäïîëàãàåò, ÷òî îí åñòü)
+
+				char client_message[DEFAULT_BUFLEN];
+
+				int client_message_length = recv(s, client_message, DEFAULT_BUFLEN, 0);
+				client_message[client_message_length] = '\0';
+
+				string check_exit = client_message;
+				if (check_exit == "off")
+				{
+					cout << "Client #" << i << " is off\n";
+					client_socket[i] = 0;
+				}
+
+				string temp = client_message;
+				temp += "\n";
+				history.push_back(temp);
+
+				for (int i = 0; i < MAX_CLIENTS; i++) {
+					if (client_socket[i] != 0) {
+						send(client_socket[i], client_message, client_message_length, 0);
+					}
+				}
+
+			}
+		}
+	}
+
+	WSACleanup();
+}
